ab606c45cea79475c2379bae1e2e0ba1128e6556
Colin <152638120+ColinZeDev@users.noreply.github.com>
2025-12-11 11:37:55 -0700

    Add project details and license notice to main.py
    Added a print statement with project details and license information.

--- main.py
+++ main.py
@@ -10,4 +10,16 @@ engine = QQmlApplicationEngine()
 engine.rootContext().setContextProperty("backend", backend)
 engine.load("gui.qml")
 
+print(
+"""
++--------------------------------------------------------------+\n
+|            Made by ColinZeDev on Github!                     |\n
+|      Repo: https://github.com/ColinZeDev/DistroVer/          |\n
+|                                                              |\n
+|      !! THIS CODE IS UNDER THE GPL-3.0 LICENSE !!            |\n
+|      !! PLEASE READ THE LICENSE FILE BEFORE MODIFYING !!     |\n
++--------------------------------------------------------------+
+"""
+)
+
 app.exec()
