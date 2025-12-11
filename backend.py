4da48190935e18eae2ecbaa4b57b65719792997c
Colin <152638120+ColinZeDev@users.noreply.github.com>
2025-12-11 10:33:54 -0700

    Implement backend to retrieve distro information and logo

---
+++ backend.py
@@ -0,0 +1,58 @@
+from PySide6.QtCore import QObject, Property
+import platform
+import distro
+import json
+import os
+
+with open('imgs.json', 'r') as f:
+    DISTRO_IMGS = json.load(f)
+
+DEFAULT_IMG = "img/linux.png"
+
+
+def get_distro_logo():
+    distro_id = distro.id().lower()
+
+    if distro_id in DISTRO_IMGS:
+        return DISTRO_IMGS[distro_id]
+
+    for like in distro.like().split():
+        if like.lower() in DISTRO_IMGS:
+            return DISTRO_IMGS[like.lower()]
+
+    return DEFAULT_IMG
+
+
+class Backend(QObject):
+    def __init__(self):
+        super().__init__()
+
+        name = distro.name()
+        version = distro.version()
+        codename = distro.codename() or "[Unknown]"
+        hostname = os.uname().nodename
+        username = os.environ.get("USER") or os.environ.get("USERNAME")
+
+        self._logo = get_distro_logo()
+
+        self._info = (
+            f"{name}\n\n"
+            f"Version {version} ({codename})\n\n"
+            f"Host name: {hostname}\n\n"
+            f"Username: {username}\n\n"
+            f"Kernel: {platform.release()}\n\n"
+        )
+
+        self._version = "0.0.2"
+
+    @Property(str, constant=True)
+    def logo(self):
+        return self._logo
+
+    @Property(str, constant=True)
+    def infoText(self):
+        return self._info
+
+    @Property(str, constant=True)
+    def version(self):
+        return self._version
